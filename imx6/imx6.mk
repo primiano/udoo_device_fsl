8b943f320436bf7df1659ce64b4927043f7a7fef
Primiano Tucci <p.tucci@gmail.com>
2015-01-18 18:48:11 +0000

    Disable strict mode

--- imx6/imx6.mk
+++ imx6/imx6.mk
@@ -330,7 +330,8 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # for property
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-	persist.sys.usb.config=mtp
+	persist.sys.usb.config=mtp \
+	persist.sys.strictmode.disable=1
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := \
         device/fsl/common/security/testkey
